3ca53b6b626c6fdbc7c392b3397d449ea4c5fbac
PabloCorcho <corchocaballeropablo@gmail.com>
2022-06-06 12:47:17 +1000

    Instrumental LSF modelling

--- src/midas/mock_observation.py
+++ src/midas/mock_observation.py
@@ -12,7 +12,9 @@ from astropy.io import fits
 from scipy.stats import binned_statistic_2d
 from smoothing_kernel import CubicSplineKernel, GaussianKernel
 from pseudoRT_model import RTModel
+from ppxf.ppxf_util import log_rebin, gaussian_filter1d
 import os
+import numpy as np
 from numpy import (
     array, logspace, linspace, arange, where, zeros, zeros_like, float32, interp,
     newaxis, sqrt, sum, cumsum, diff, mean, median, cross, dot, sin, cos,
@@ -40,10 +42,16 @@ class WEAVE_Instrument(Instrument):
     configfile = os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
         'cube_templates', 'WEAVE', 'weave_instrument.yml')
+    wl_lsf, lsf = np.loadtxt(os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        'cube_templates', 'WEAVE', 'weave_lsf'))
 
     def __init__(self, **kwargs):
-        print('-' * 50 + '\n [INSTRUMENT] Initialising instrument: WEAVE LIFU\n' + '-' * 50)
-        print('\n [INSTRUMENT] Loading default parameters for WEAVE LIFU\n -- configfile: ' + self.configfile)
+        print('-' * 50
+              + '\n [INSTRUMENT] Initialising instrument: WEAVE LIFU\n'
+              + '-' * 50)
+        print('\n [INSTRUMENT] Loading default parameters for WEAVE LIFU\n -- configfile: '
+              + self.configfile)
         with open(self.configfile, 'r') as file:
             config = yaml.load(file, Loader=yaml.FullLoader)
             self.field_of_view = tuple(config['fov']) * u.arcsec  # arcsec
@@ -55,37 +63,48 @@ class WEAVE_Instrument(Instrument):
             self.pixel_size = config['pixel_size'] * u.arcsec
 
         # Build the wavelenth array
-        self.wavelength = arange(self.wave_init, self.wave_end, self.delta_wave) * u.angstrom
+        self.wavelength = arange(self.wave_init, self.wave_end,
+                                 self.delta_wave) * u.angstrom
         # Wavelenght array for blue and red arms (used for saving each cube separately)
-        self.wavelength_blue = arange(self.wave_init, self.wave_init + self.delta_wave * self.blue_arm_npix,
-                                      self.delta_wave) * u.angstrom
-        self.wave_blue_pos = where(self.wavelength <= self.wavelength_blue[-1])[0]
-        self.wavelength_red = arange(self.wave_end - self.delta_wave * self.red_arm_npix, self.wave_end,
-                                      self.delta_wave) * u.angstrom
+        self.wavelength_blue = arange(
+            self.wave_init,
+            self.wave_init + self.delta_wave * self.blue_arm_npix,
+            self.delta_wave) * u.angstrom
+        self.wave_blue_pos = where(
+            self.wavelength <= self.wavelength_blue[-1])[0]
+        self.wavelength_red = arange(
+            self.wave_end - self.delta_wave * self.red_arm_npix, self.wave_end,
+            self.delta_wave) * u.angstrom
         self.wave_red_pos = where(self.wavelength >= self.wavelength_red[0])[0]
         self.wavelength_edges = arange(self.wave_init - self.delta_wave / 2,
                                        self.wave_end + self.delta_wave / 2,
                                        self.delta_wave) * u.angstrom
-        self.wavelength_arms = {'BLUE': self.wavelength_blue, 'RED': self.wavelength_red}
+        self.wavelength_arms = {'BLUE': self.wavelength_blue,
+                                'RED': self.wavelength_red}
 
-        print('\n  · FoV: ({:.1f}, {:.1f})'.format(self.field_of_view[0], self.field_of_view[1])
+        print('\n  · FoV: ({:.1f}, {:.1f})'.format(self.field_of_view[0],
+                                                   self.field_of_view[1])
               + '\n  · Pixel size: {:.2f}'.format(self.pixel_size)
               + '\n  · Delta(Lambda) (angstrom): {:.1f}'.format(self.delta_wave)
-              + '\n  · Wavelength range (angstrom): {:.2f} - {:.2f}'.format(self.wave_init, self.wave_end)
+              + '\n  · Wavelength range (angstrom): {:.2f} - {:.2f}'.format(
+                  self.wave_init, self.wave_end)
               + '\n    -> Blue arm: {:.2f} - {:.2f} ({} pixels)'.format(
-            self.wavelength_blue[0], self.wavelength_blue[-1], self.wavelength_blue.size)
+            self.wavelength_blue[0], self.wavelength_blue[-1],
+            self.wavelength_blue.size)
               + '\n    -> Red arm: {:.2f} - {:.2f} ({} pixels)'.format(
-            self.wavelength_red[0], self.wavelength_red[-1], self.wavelength_red.size)
-              )
+            self.wavelength_red[0], self.wavelength_red[-1],
+            self.wavelength_red.size))
 
         self.redshift = kwargs.get('z', 0.05)
         self.get_pixel_physical_size()
         self.detector_bins()
         print('\n [INSTRUMENT] specific parameters for observation'
               + '\n  · Source redshift: {:.4f}'.format(self.redshift)
-              + '\n  · Pixel physical size: {:.4f}'.format(self.pixel_size_kpc))
+              + '\n  · Pixel physical size: {:.4f}'.format(self.pixel_size_kpc)
+              )
 
     def detector_bins(self):
+        """todo."""
         self.det_x_n_bins = int(self.field_of_view[0].value
                                 / self.pixel_size.value)
         self.det_y_n_bins = int(self.field_of_view[1].value
@@ -106,12 +125,14 @@ class WEAVE_Instrument(Instrument):
                                + self.det_y_bin_edges_kpc[1:]) / 2
 
     def get_pixel_physical_size(self):
+        """todo."""
         ang_dist = cosmo.angular_diameter_distance(self.redshift
                                                    ).to('kpc')
         self.pixel_size_kpc = (ang_dist.value
                                * self.pixel_size.to('radian').value) * u.kpc
 
     def bin_particles(self):
+        """todo."""
         self.stat, _, _, self.binnumb = binned_statistic_2d(
             x=self.X, y=self.Y, values=None,
             statistic='count',
@@ -128,6 +149,8 @@ class WEAVE_Instrument(Instrument):
 
 class Galaxy(object):
     """
+    Def.
+
     This class represents a galaxy from IllustrisTNG simulations and contains
     different elements such as stellar and gas particles.
     """
@@ -143,6 +166,7 @@ class Galaxy(object):
         self.position = kwargs.get('gal_pos', zeros(3))  # kpc/(km/s)
 
     def build_stars(self):
+        """todo."""
         self.stars = {}
         if self.stars_params is not None:
             for key in list(self.stars_params.keys()):
@@ -153,12 +177,14 @@ class Galaxy(object):
                 scale_f[::-1], age_f[::-1])
 
     def build_gas(self):
+        """todo."""
         self.gas = {}
         if self.gas_params is not None:
             for key in list(self.gas_params.keys()):
                 self.gas[key] = self.gas_params[key][()]
 
     def set_to_galaxy_rest_frame(self):
+        """todo."""
         if self.stars_params is not None:
             self.stars['Velocities'] += -self.velocity[newaxis, :]
             self.stars['Coordinates'] += -self.position[newaxis, :]
@@ -169,6 +195,7 @@ class Galaxy(object):
         self.position = zeros(3)
 
     def proyect_galaxy(self, orthogonal_vector):
+        """todo."""
         norm = orthogonal_vector / sqrt(sum(orthogonal_vector**2))
         self.proyection_vector = norm
         b = cross(array([0, 0, 1]), norm)
@@ -225,19 +252,26 @@ class Galaxy(object):
 
 
 class ObserveGalaxy(object):
+    """todo."""
+
     def __init__(self, SSP, Instrument, Galaxy, kernel_args=None):
-        print('-' * 50 + '\n [OBSERVATION]  Initialising observation\n' + '-' * 50)
+        print('-' * 50 + '\n [OBSERVATION]  Initialising observation\n'
+              + '-' * 50)
         # Simple Stellar Populations Model for generating synthetic SEDs
         self.SSP = SSP
         # Instrument that will observe the galaxy
         self.instrument = Instrument
-        
+
         # Interpolate SSP models to Instrumental resolution
         print(' [Observation]  Interpolating SSP models to instrumental resolution')
+        self.SSP.cut_models(self.instrument.wavelength_edges.min() * .9, self.instrument.wavelength_edges.max() * 1.1)
         self.SSP.interpolate_sed(self.instrument.wavelength_edges)
+        self.lsf_sigma = np.interp(self.SSP.wavelength.value, self.instrument.wl_lsf, self.instrument.lsf)
+        self.SSP.convolve_sed(profile=gaussian_filter1d,
+                              **dict(sig=self.lsf_sigma / np.median(np.diff(self.SSP.wavelength.value))))
         # Galaxy to be observed
         self.galaxy = Galaxy
-        # Resultant synthetic cube
+        # Synthetic cube
         self.cube = zeros((self.instrument.wavelength.size,
                            self.instrument.det_x_n_bins,
                            self.instrument.det_y_n_bins
@@ -252,27 +286,30 @@ class ObserveGalaxy(object):
 
     def compute_los_emission(self, stellar=True, nebular=False,
                              dust_extinction=None, kinematics=False):
+        """Compute the emission along the LOS."""
         if stellar:
             ssp_metallicities = self.SSP.metallicities
             ssp_ages = self.SSP.ages
             ssp_wave = self.SSP.wavelength
             n_stellar_part = self.galaxy.stars['Masses'].size
+            # Store the stellar masses to provide the SFH
             self.star_formation_history = zeros((self.cube.shape[1],
                                                  self.cube.shape[2],
                                                  ssp_ages.size)) * u.Msun
+            # Store stellar kinematics
             self.velocity_field = zeros((self.cube.shape[1],
                                          self.cube.shape[2])
                                         )
-
+            # Initialise pseudo radiative transfer element
             self.rtmodel = RTModel(wave=ssp_wave,
                                    redshift=self.instrument.redshift,
                                    galaxy=self.galaxy)
 
             print(' [Observation] Computing stellar spectra for'
-                  +' {} star particles'.format(n_stellar_part))
+                  + ' {} star particles'.format(n_stellar_part))
             part_i = 0
             while part_i < n_stellar_part:
-                print("\r Completion --> {}, {:2.2f} %".format(
+                print("\r Particle --> {}, Completion: {:2.2f} %".format(
                     part_i, part_i/n_stellar_part * 100), end='', flush=True)
                 mass, age, metals = (
                     self.galaxy.stars['GFM_InitialMass'][part_i] * 1e10 * u.Msun,
@@ -292,7 +329,7 @@ class ObserveGalaxy(object):
                 # NH, Z = self.rtmodel.compute_nh_column_density(
                 #     x_pos, y_pos, z_pos)
                 # ext = self.rtmodel.compute_extinction(
-                #     Z_gas=Z, N_hydrogen=NH)
+                #     Z_gas=Z, N_hyldrogen=NH)
                 # sed_extinction = sed * ext * u.angstrom / u.angstrom
                 # Kernel Smoothing
                 r = sqrt((self.X_kpc - x_pos)**2
@@ -301,11 +338,11 @@ class ObserveGalaxy(object):
                 # (blue/red)shifting spectra
                 vel_z = self.galaxy.stars['ProjVelocities'][2, part_i]
                 redshift = vel_z / 3e5
-                wave = ssp_wave * (1+redshift)
+                wave = ssp_wave * (1 + redshift)
                 # Interpolation to instrumental resolution
                 cumulative = cumsum(sed * diff(wave)[0])
                 sed = interp(self.instrument.wavelength_edges, wave,
-                             cumulative, left=0.0, right=0.0)
+                             cumulative)
                 sed = diff(sed) / (self.instrument.delta_wave * u.angstrom)
 
                 # cumulative_extinction = cumsum(sed_extinction
@@ -334,6 +371,7 @@ class ObserveGalaxy(object):
         self.luminosity_to_flux()
 
     def luminosity_to_flux(self):
+        """todo."""
         print(' [Observation] Converting luminosities to observed fluxes for'
               + ' a galaxy located at z={:.4f}'.format(
                   self.instrument.redshift))
@@ -343,6 +381,7 @@ class ObserveGalaxy(object):
         self.cube_extinction = self.cube_extinction / (4 * pi * L_dist**2)
 
     def add_noise(self):
+        """todo."""
         self.noise = (2*random.rand(self.cube.size)-1).reshape(
             self.cube.shape
             ) * 1e-17 * (u.erg/u.s/u.angstrom)
