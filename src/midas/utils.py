f6cb65deaeff6f9853a0f5566cee3684d08156ed
PabloCorcho <corchocaballeropablo@gmail.com>
2023-04-12 11:56:27 +0200

    2d interpolation method included

--- src/midas/utils.py
+++ src/midas/utils.py
@@ -12,6 +12,27 @@ def fast_interpolation(f, dx, x, xnew, dxnew):
     fnew = np.diff(interpolation) / dxnew
     return fnew
 
+def inter_2d(array, x_range, y_range, x_inter, y_inter, extrapolate=0.0):
+    """"This function performs 2D linear interpolation on a given 2D array."""
+    if (x_inter > max(x_range) or x_inter < min(x_range)
+        or y_inter > max(y_range) or y_inter < min(y_range)):
+        print("Value out of range")
+        return extrapolate
+    else:
+        indx1 = np.searchsorted(x_range, x_inter)
+        indx2 = np.searchsorted(y_range, y_inter)
+        
+        # weights
+        w1 = (x_inter - x_range[indx1 - 1]) / (x_range[indx1] - x_range[indx1 - 1])
+        w2 = (y_inter - y_range[indx2 - 1]) / (y_range[indx2] - y_range[indx2 - 1])
+    
+        interp_array =  (w1  * w2 * array[indx1, indx2,:]
+                + (1 - w1) * w2 * array[indx1 - 1, indx2, :]
+                + w1 * (1 - w2) * array[indx1, indx2 - 1, :]
+                + (1 - w1) * (1 - w2) * array[indx1 - 1, indx2 - 1, :])
+        return interp_array
+
+
 if __name__ == '__main__':
     from time import time
     import numpy as np
