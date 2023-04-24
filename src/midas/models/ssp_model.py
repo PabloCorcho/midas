cc55b92d0476a7fda05370471be0d9d180bd7414
PabloCorcho <corchocaballeropablo@gmail.com>
2023-04-24 09:18:49 +0200

    commonalities between classes

--- src/midas/models/ssp_model.py
+++ src/midas/models/ssp_model.py
@@ -24,39 +24,7 @@ class SSP_model(BidimensionalGridMixin, EmissionGridModel):
                       self.metallicities, age, metallicity,
                       log=True)
         return s * mass
-
-    def interpolate_sed(self, new_wl_edges):
-        """Flux-conserving interpolation.
-
-        params
-        -----
-        - new_wl_edges: bin edges of the new interpolated points.
-        """
-        new_wl = (new_wl_edges[1:] + new_wl_edges[:-1]) / 2
-        dwl = np.diff(new_wl_edges)
-        ori_dwl = np.hstack(
-            (np.diff(self.wavelength),
-                self.wavelength[-1] - self.wavelength[-2]))
-        print(' [SSP] Interpolating SSP SEDs')
-        new_sed = np.empty((new_wl.size, *self.sed_grid.shape[1:]),
-                           dtype=float)
-        for i in range(self.sed_grid.shape[1]):
-            for j in range(self.sed_grid.shape[2]):
-                f = np.interp(new_wl_edges, self.wavelength,
-                                np.cumsum(self.sed_grid[:, i, j] * ori_dwl))
-                new_flux = np.diff(f) / dwl
-                new_sed[:, i, j] = new_flux
-        self.sed_grid = new_sed
-        self.wavelength = new_wl
-
-    def convolve_sed(self, profile=gaussian1d_conv, **profile_params):
-        """Convolve the SSP spectra with a given LSF."""
-        print(' [SSP] Convolving SSP SEDs')
-        for i in range(self.sed_grid.shape[1]):
-            for j in range(self.sed_grid.shape[2]):
-                self.sed_grid[:, i, j] = profile(self.sed_grid[:, i, j],
-                                                **profile_params)
-
+    
     def get_mass_lum_ratio(self, wl_range):
         """Compute the mass-to-light ratio within a giveng wavelength range."""
         pts = np.where((self.wavelength >= wl_range[0]) &
